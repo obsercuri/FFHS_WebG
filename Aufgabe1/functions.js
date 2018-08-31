1c41ba9cf37b13a04aabb8d78ade20c5c6721665
obsercuri <stephan.graber@gmail.com>
2018-08-31 13:39:43 +0200

    Update functions.js

--- Aufgabe1/functions.js
+++ Aufgabe1/functions.js
@@ -1,15 +1,16 @@
 function reverseString(str) {
+    // check if string is empty
     if (str === "")
-        return "";
+        return ""; // terminates recursion
     else
-        return reverseString(str.substr(1)) + str.charAt(0);
+        return reverseString(str.substr(1)) + str.charAt(0); // each call differnt char at 0 and put it back
 }
 
 function leapYear(year) {
-    if (year % 4 === 0)
-        if (year % 400 === 0)
+    if (year % 4 === 0) // check if year is divadable by 4
+        if (year % 400 === 0) // check if year is divadable by 400
             return "This is a leap year";
-        else if (year % 100 === 0)
+        else if (year % 100 === 0) // check if year is divadable by 100
             return "This is no leap year";
         else
             return "This is a leap year";
@@ -18,7 +19,7 @@ function leapYear(year) {
 }
 
 function isPangram(string){
-    if (/(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(string))
+    if (/(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(string)) //regex to check if each char is included in the string
         return string + " is a pangram";
     else
         return string + " is not a pangram";
