e58399c9a4f8a42d0d56cac6b71870692b96eb48
obsercuri <stephan.graber@gmail.com>
2018-09-07 15:52:45 +0200

    Update functions.js
    Code golfing try ;)
    -> return boolean for the last two functions

--- Aufgabe1/functions.js
+++ Aufgabe1/functions.js
@@ -7,21 +7,11 @@ function reverseString(str) {
 }
 
 function leapYear(year) {
-    if (year % 4 === 0) // check if year is divadable by 4
-        if (year % 400 === 0) // check if year is divadable by 400
-            return "This is a leap year";
-        else if (year % 100 === 0) // check if year is divadable by 100
-            return "This is no leap year";
-        else
-            return "This is a leap year";
-    else
-        return "This is no leap year!";
+    //checks if the 2nd month (February) has 29 days
+    return new Date(year, 1, 29).getDate() === 29;
 }
 
-function isPangram(string){
-    if (/(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(string)) //regex to check if each char is included in the string
-        return string + " is a pangram";
-    else
-        return string + " is not a pangram";
-    //return /(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(string)
+function isPangram(string) {
+    // Checks with RegEx if the string is a pangram
+    return /(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(string)
 }
